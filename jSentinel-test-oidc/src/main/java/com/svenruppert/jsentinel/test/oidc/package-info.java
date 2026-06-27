e1ff2576f8d330132e8bea58df58a331abe1d7d0
Sven Ruppert <sven.ruppert@gmail.com>
2026-06-27 08:32:17 +0200

    chore(00.79.00/000-001): bump 00.79.00-SNAPSHOT + scaffold 7 Etappe-1 modules
    P000: versions:set 00.78.00-SNAPSHOT -> 00.79.00-SNAPSHOT across all 47 poms.
    P001: scaffold jSentinel-test-oidc (OIDC test infra) + the six
    jSentinel-identity-vendor-{keycloak,entra,auth0,okta,google,github} profile
    modules; registered in parent <modules> (now 53). Reactor green.

---
+++ jSentinel-test-oidc/src/main/java/com/svenruppert/jsentinel/test/oidc/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright © 2017 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence"); You may not use this work except in
+ * compliance with the Licence. You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+/**
+ * V00.79 OIDC test infrastructure: StubIdentityProvider, MockClock and integration-test helpers for exercising the OIDC RP end to end without mocks.
+ *
+ * @since 00.79.00
+ */
+package com.svenruppert.jsentinel.test.oidc;
