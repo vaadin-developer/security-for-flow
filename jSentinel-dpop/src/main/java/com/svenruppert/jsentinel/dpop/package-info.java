fffa91587e6377b3503e1304e533585966447daf
Sven Ruppert <sven.ruppert@gmail.com>
2026-06-27 11:40:27 +0200

    chore(00.79.10/000-001): bump 00.79.10-SNAPSHOT + scaffold jSentinel-dpop

---
+++ jSentinel-dpop/src/main/java/com/svenruppert/jsentinel/dpop/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2017 Sven Ruppert (sven.ruppert@gmail.com)
+ *
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence"); You may not use this work except in
+ * compliance with the Licence. You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+/**
+ * V00.79 DPoP (RFC 9449) — proof generator (outbound) and validator (inbound) with
+ * htm/htu/iat/jti checks, JWK-thumbprint cnf binding and jti replay protection.
+ *
+ * @since 00.79.10
+ */
+package com.svenruppert.jsentinel.dpop;
